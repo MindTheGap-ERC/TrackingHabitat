3588dc3d92d3abf617cf1d4c82c11ad62de89e07
Liu <x.liu6@uu.nl>
2025-11-17 15:11:02 +0100

    generate a sin sealevel curve

---
+++ src/GenerateTopo/generate_sealevel.jl
@@ -0,0 +1,14 @@
+using DelimitedFiles
+
+n_points = 1000
+amplitude_a = 10.0 
+amplitude_b = 5.0         
+periods_a = 5             
+periods_b = 2   
+x = range(0.0, stop=1.0, length=n_points)
+
+y = amplitude_a .* sin.(2pi .* periods_a .* x) .+ amplitude_b .* sin.(2pi .* periods_b .* x)
+
+
+# save: separate files (no header), and a combined file (x, y5, y2)
+writedlm("src/Stacker/parameters/sealevel_sin.txt", y')
