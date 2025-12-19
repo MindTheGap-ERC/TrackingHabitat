b42ded1b63c84db3e83bec7a75879ed5b990fc2a
Liu <x.liu6@uu.nl>
2025-12-19 11:59:50 +0100

    draw real cores

--- src/PlottingCores/plottingcores.jl
+++ src/PlottingCores/plottingcores.jl
@@ -1,3 +1,4 @@
+module PlottingCores
 using MAT
 using CairoMakie
 
@@ -7,7 +8,7 @@ stratadata = data["strata"]
 params = data["params"]
 
 #extract data
-facies = stratadata["facies"]
+facies = stratadata["facies"] 
 layers = stratadata["layers"]
 
 # how much facies we have?
@@ -17,6 +18,7 @@ facies_code = unique(facies)
 new_facies_map = Dict(0.0=>100, 1.0=>101, 2.0=>102, 3.0=>103, 4.0=>103, 5.0=>105)
 reproj_facies = map(x -> get(new_facies_map, x, x), facies)
 unique(reproj_facies)
+
 # faciesMosaicElementTrajectoryY is 25*10000: rows are each patch, columns are time steps
 # faciesMosaicElementYInc is 25*1: rows are each patch, columns are y increment per time step
 
@@ -34,36 +36,47 @@ function get_facies_alpha(facies_code)
     return get(facies_alphas, facies_code, 1.0)
 end
 
-function draw_sub_core!(ax, layers, facies, core_x, core_y)
+function draw_sub_core!(ax, layers::Array{Float64}, facies::Array{Int64}, core_idx, core_coords)
+    core_x, core_y = core_coords[core_idx]
     core_layers = layers[core_x, core_y, :]
     core_facies = facies[core_x, core_y, :]
-    core_layers_norm = core_layers .- core_layers[1] 
+    subcore!(ax, core_layers, core_facies)
 
+end
+
+function draw_sub_core!(ax, layers::Vector{<:Vector}, facies::Vector{<:Vector}, core_idx, core_coords)
+    core_layers = layers[core_idx]
+    core_facies = facies[core_idx]
+    println("calling realcorefunction")
+    subcore!(ax, core_layers, core_facies)
+end
+
+function subcore!(ax, core_layers, core_facies)
+    core_layers_norm = core_layers .- core_layers[1] 
     for idx in eachindex(core_layers_norm)[2:end]
         top = core_layers_norm[idx]
         bottom = core_layers_norm[idx-1]
-        if core_facies[idx] != 100
-            hspan!(ax, bottom, top; color = get_facies_color(core_facies[idx]), alpha = get_facies_alpha(core_facies[idx]))
-        end
-    end
+        
+        hspan!(ax, bottom, top; color = get_facies_color(core_facies[idx]), alpha = get_facies_alpha(core_facies[idx]))
 
+    end
 end
 
 function draw_multiple_cores(layers, facies, core_coords)
     n_cores = length(core_coords)
     fig = Figure(resolution = (200 * n_cores, 600))
-    for (i, (core_x, core_y)) in enumerate(core_coords)
+    for i in eachindex(core_coords)
         if i == 1
             ax = Axis(fig[1, i]; xlabel = "", ylabel = "Depth (m)", yreversed = false, xticks = ([], []))
         else
             ax = Axis(fig[1, i]; xlabel = "", ylabel = "", yreversed = false, xticks = ([], []))
         end
-        draw_sub_core!(ax, layers, facies, core_x, core_y)
+        draw_sub_core!(ax, layers, facies, i, core_coords)
         
     end
 
-    facies_codes = [100, 101, 102, 103, 105]
-    facies_labels = ["hiatus","grainstone", "skeletal grainstone", "mud-lean packstone", "packstone"]
+    facies_codes = [101, 102, 103, 105]
+    facies_labels = ["ooidal grainstone", "skeletal grainstone", "ooidal packstone", "packstone"]
     legend_elements = [PolyElement(color = get_facies_color(code)) for code in facies_codes]
     Legend(fig[1, length(core_coords) + 1], legend_elements, facies_labels)
 
@@ -74,3 +87,4 @@ end
 fig = draw_multiple_cores(layers, reproj_facies, core_coords) 
 save("fig/STACKER_cores.png", fig)
 
+end
